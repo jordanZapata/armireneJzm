6e2e8ad2f8c98244a0c33a8d6e346eee39eea59e
jordan zapata <jordan199442@gmail.com>
2023-12-29 11:06:49 -0500

    Se resolvio un problema de decorador para poder hacer el Get, no fue implementacion adicional, puede ser tenido encuenta o no para la evaluacion de la prueba

--- armireneJzm/src/main/java/com/pruebaTecnica/armireneJzm/controller/UserController.java
+++ armireneJzm/src/main/java/com/pruebaTecnica/armireneJzm/controller/UserController.java
@@ -4,14 +4,14 @@ import com.pruebaTecnica.armireneJzm.model.UserModel;
 import com.pruebaTecnica.armireneJzm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
